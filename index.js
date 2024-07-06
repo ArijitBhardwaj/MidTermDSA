bd7f42927efd33ea2246d27a4f3e74f336eeac40
Arijit Bhardwaj <000461010@student.vcc.ca>
2024-07-05 19:46:01 -0700

    Edited after checking for tests

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 
 
     function missingNumber(array){
-        for (let i of array){
-            let size = array.length;
-            let i = 1;
-           while(array.indexOf(i) && i<= size ){
+        let size = array.length;
+        
+             let i = 1;
+           while(array.includes(i) && i<= size ){
                 i++;
                 }
                 return i;
-            }
+            
         }
     // Time complexity = O(n) (Worst Case scenario - iterating through all elements once)
     
     
-        function sumOfTwo (array, target){
+       function sumOfTwo (array, target){
             let answer = [];
             for ( let i in array){
                 if( array[i] < target){
                     let diff = target - array[i];
-                    if(j = indexOf(diff)){
-                        answer.push(j);
-                        answer.push(i);
+                    if(array.indexOf(diff)){
+                        answer.push(array.indexOf(diff));
+                         answer.push(i);
                         return answer;
                     }
                 }
@@ -37,7 +37,7 @@
     for (let i = 0; i < str.length; i++) {
       const currentChar = str[i];
       const remainingChars = str.slice(0, i) + str.slice(i + 1);
-      const remainingCharsPermuted = findPerms(remainingChars);// this step happens (n-1)! times
+      const remainingCharsPermuted = findPermutations(remainingChars);// this step happens (n-1)! times
       for (let j = 0; j < remainingCharsPermuted.length; j++){ 
         const permutation = currentChar + remainingCharsPermuted[j]// this step happens (n-1) times because of the j loop
         result.push(permutation)
@@ -62,54 +62,55 @@
     }
 
     prepend(data){
-        newNode = new Node(data);
+        var newNode = new Node(data);
         newNode.next = this.head;
         this.head = newNode;
-        size++;
+        this.size++;
 
     }
 
 
     checkIfCycleExists(headNode){
-        if(size ===0){return "List is Empty";}
-        if(size === 1){return "List has single element";}
+        if(this.size ===0){return false;}
+        if(this.size === 1){return false;}
         let current = this.head.next;
-        while(current.next){
+        while(current){
             if (current.data === headNode.data){
                 return true;
             }
-            current = current.next;
+            else{current = current.next;}
+            
         }
+        return false;
     }
   }
 
   // Time Complexity is O(n) - traversing in a linked list
 
 
-  function isStrValid(str){
-    if(str === ''){
-        return true;
-    }
+function checkIfValidParenthesis(str) {
+    let stack = [];
+    const bracketPairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
 
-    let map = {')' : '(', '}' : '{', ']' : '['};
-    var check = [];
-
-    for (var c in str){
-        if (c in '({['){
-            check.push(c);
-
-        }
-
-        else {
-            if (map[c] in check){
-                check.filter(function (letter){
-                    return letter !==map[c];
-                });
-            } 
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack[stack.length - 1] !== bracketPairs[char]) {
+                return false;
+            } else {
+                stack.pop();
+            }
         }
     }
 
-    return !check.length;
-  }
+    return stack.length === 0;
+}
+
 
   //Time Complexity - O(n) since it will iterate through every character of the string 
